c35d151fb1917307f4202fe16d2287fcadfdb0f3
Maria Consuelo Acero Martinez <maceroma@everis.com>
2020-09-04 17:54:13 -0500

    Updates

---
+++ precache-manifest.51ac0c86d9c3e6a112d52cce55d2e007.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1beae7243f49bb4f3e022ff01fb0cb63",
+    "url": "/github.io/store-acero-maria/index.html"
+  },
+  {
+    "revision": "6c3f04b8ca50fa033c9a",
+    "url": "/github.io/store-acero-maria/static/css/main.79e7f76d.chunk.css"
+  },
+  {
+    "revision": "b85a3178775e3b8d4df0",
+    "url": "/github.io/store-acero-maria/static/js/2.556c91f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/github.io/store-acero-maria/static/js/2.556c91f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3f04b8ca50fa033c9a",
+    "url": "/github.io/store-acero-maria/static/js/main.7543ce1f.chunk.js"
+  },
+  {
+    "revision": "a29fc6911f8b2a7b35ad",
+    "url": "/github.io/store-acero-maria/static/js/runtime-main.e06b28e3.js"
+  },
+  {
+    "revision": "2f11a957b72ff4a5c46227027cf46115",
+    "url": "/github.io/store-acero-maria/static/media/buy-blue.2f11a957.svg"
+  }
+]);
